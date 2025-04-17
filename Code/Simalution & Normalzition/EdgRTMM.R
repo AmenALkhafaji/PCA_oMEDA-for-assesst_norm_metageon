a866f459081c43b6a754aadf78a3c0adb6b8e50e
AmenALkhafaji <162328890+AmenALkhafaji@users.noreply.github.com>
2025-04-17 13:45:40 +0300

    Update EdgRTMM.R

--- Code/Simalution & Normalzition/EdgRTMM.R
+++ Code/Simalution & Normalzition/EdgRTMM.R
@@ -8,7 +8,7 @@ if (!requireNamespace("edgeR", quietly = TRUE)) {
 library(edgeR)
 
 # Read the CSV file, assuming the last column is the group info
-data <- read.csv("D:\\Genus.csv", header = TRUE, sep=";")
+data <- read.csv("Phylum.csv", header = TRUE, sep=";")
 
 # Extract the group information from the 37th column
 group <- as.factor(data[, 37])  # Assuming 1 = h, 2 = S
@@ -26,5 +26,5 @@ dge <- calcNormFactors(dge, method = "TMM")
 normalized_counts <- cpm(dge, log = FALSE)
 
 # Save normalized counts to CSV file
-write.csv(normalized_counts, file = "D:\\edgRTMM.csv", row.names = TRUE)
+write.csv(normalized_counts, file = "EdgRTMM.csv", row.names = TRUE)
 
