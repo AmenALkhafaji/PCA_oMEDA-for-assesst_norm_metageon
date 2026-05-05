68a85d9816a26ca576da5dbed43dc0a6a7f572c6
AmenALkhafaji <162328890+AmenALkhafaji@users.noreply.github.com>
2026-05-05 12:38:05 +0300

    Delete Code directory

--- Code/GenusData/csv/CSVALL.m
+++
@@ -1,4 +0,0 @@
-% Read the CSV file
-data = readtable('CSS_Rarefaction_Genus.csv');
-microbiome_data = table2array(data(1:end, :));
-save('C:\Users\AL-GHADIR\Desktop\table6\MEDA-Toolbox-1.6\MEDA-Toolbox-1.6\GenusData\data\CSS_Rarefaction_Genus.mat', 'microbiome_data');
--- Code/PhylumData/csv/CSVall.m
+++
@@ -1,6 +0,0 @@
-% Read the CSV file
-data = readtable('Deseq_rarefaction.csv');
-microbiome_data = table2array(data(1:end,: ));
-
-
-save('C:\Users\AL-GHADIR\Desktop\table6\MEDA-Toolbox-1.6\MEDA-Toolbox-1.6\PhylumData\data\Deseq_rarefaction.mat', 'microbiome_data');
