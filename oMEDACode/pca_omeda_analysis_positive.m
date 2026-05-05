7154fe005fcf82e30214b96ce07f9bd9a3571906
AmenALkhafaji <162328890+AmenALkhafaji@users.noreply.github.com>
2026-05-05 12:48:43 +0300

    Add files via upload

---
+++ oMEDACode/pca_omeda_analysis_positive.m
@@ -0,0 +1,8 @@
+Normalization_Method,control shannon_mean(sd) at low depth,Low case shannon mean( sd),Counts of sig p_value at low depth,Counts of nonsig p_value at low depth,control shannon mean( sd) at high depth,case shannon mean( sd) high,Counts of sig p_value at high depth,Counts of nonsig p_value at high depth,control shannon mean( sd) at full depth,case shannon mean( sd) at full depth,Count of sig p_value at full depth,Counts of nonsig p_value at full depth
+CLR,1.0417 (0.0077),1.0401 (0.0081),8,92,1.0185 (0.0056),1.0179 (0.0055),0,100,1.0283 (0.0040),1.0284 (0.0038),5,95
+CSS,0.9796 (0.0089),0.9776 (0.0092),7,93,0.9872 (0.0060),0.9865 (0.0057),1,99,0.9832 (0.0043),0.9832 (0.0042),6,94
+DESeq2,0.9796 (0.0089),0.9776 (0.0092),7,93,0.9872 (0.0060),0.9865 (0.0057),1,99,0.9832 (0.0043),0.9832 (0.0042),6,94
+RAREFY,0.9741 (0.0096),0.9734 (0.0102),5,95,0.9733 (0.0105),0.9739 (0.0098),8,92,0.9731 (0.0059),0.9739 (0.0065),8,92
+TSS,0.9796 (0.0089),0.9776 (0.0092),7,93,0.9872 (0.0060),0.9865 (0.0057),1,99,0.9832 (0.0043),0.9832 (0.0042),6,94
+counts,0.9796 (0.0089),0.9776 (0.0092),7,93,0.9872 (0.0060),0.9865 (0.0057),1,99,0.9832 (0.0043),0.9832 (0.0042),6,94
+edgeR,0.9796 (0.0089),0.9776 (0.0092),7,93,0.9872 (0.0060),0.9865 (0.0057),1,99,0.9832 (0.0043),0.9832 (0.0042),6,94
